5178c39780b384184f1f74fc0f50ff53ca1f6fb0
Meong <fauzan@eenthlh.xyz>
2020-10-09 13:33:43 +0700

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -82,6 +82,7 @@ module.exports = {
     plugins: [
         ['@vuepress/google-analytics', { 'ga': 'UA-164440520-1' }],
         ['feed', feed_options ],
+        ['@vuepress/pwa', { serviceWorker: true, updatePopup: true }]
         ['sitemap', { hostname: 'https://blog.eenthlh.xyz/'}]
     ]
 }
